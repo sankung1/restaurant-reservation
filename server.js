2e59f48b4b24ab995ea48f4737ade64bdd053b45
TheMattyDGWU <themattydgwu@gmail.com>
2020-10-24 15:14:40 -0400

    updated post functions and constants

--- server.js
+++ server.js
@@ -1,51 +1,39 @@
-// const http = require("http");
-
-
-// const PORT = 8080;
-
-// function handleRequest(request, response) {
-//     response.end("It Works!! Path Hit: " + request.url);
-// }
-
-// const server = http.createServer(handleRequest);
-
-// server.listen(PORT, function() {
-//     console.log("Server listening on: http://localhost:" + PORT);
-// });
-
 const express = require("express");
 const path = require("path");
 
 const app = express();
 const PORT = 3000;
 
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 const makeRes = [{
     name: "",
     phoneNum: "",
     email: "",
     uniqueID: ""
-
 }]
 
 const availableSeats = []
 const waitingList = []
 
 // route to send the user to the ajax page
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "home.html"))
 })
 app.get("/tables", (req, res) => {
-    res.sendFile(path.join(__dirname, "table.html"))
+    res.sendFile(path.join(__dirname, "tables.html"))
 })
 app.get("/reserve", (req, res) => {
     res.sendFile(path.join(__dirname, "reserve.html"))
 })
 
-app.get("/api/makeReservation/:makeRes", (req, res)=>{
+app.get("/api/makeReservation/:makeRes", (req, res) => {
 
 })
 
 
-app.listen(PORT, () =>{
+app.listen(PORT, () => {
     console.log(`App is listening on PORT ${PORT}`);
 });
